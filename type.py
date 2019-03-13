9d2cdd0426dd329727430ec78386b4cf88fcbb77
Diku-chan <kdv99@mail.ru>
2019-03-14 00:38:37 +0300

    Added posneg classification

--- type.py
+++ type.py
@@ -1,5 +1,4 @@
 import os
-
 import pandas as pd
 import numpy as np
 
@@ -7,10 +6,8 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
-
 df_train = pd.read_csv(os.path.join('data', 'train_data.csv'), encoding='utf-8')
 df_test = pd.read_csv(os.path.join('data', 'test_data.csv'), encoding='utf-8')
-
 
 sentences_train = df_train['text'].values + df_train['title'].values
 sentences_test = df_test['text'].values + df_test['title'].values
@@ -28,12 +25,7 @@ classifier = LogisticRegression()
 classifier.fit(X_train, y_train)
 # score = classifier.score(X_test, y_test)
 # print('Accuracy for {} data: {:.4f}'.format('source', score))
-print(X_train)
 
 answer = classifier.predict(X_test)
-
-print(len(answer), type(answer), answer)
-
 df_answer = pd.DataFrame({'type':  answer})
-
-df_answer.to_csv('lol.csv')
+df_answer.to_csv('type.csv')
