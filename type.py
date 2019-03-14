37aeb45d25fac125cb77089082a9278d562659bd
Mickey <st063775@student.spbu.ru>
2019-03-15 02:30:08 +0300

    Update type.py

--- type.py
+++ type.py
@@ -1,31 +1,50 @@
 import os
+
 import pandas as pd
 import numpy as np
 
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import SGDClassifier
 from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
 
-df_train = pd.read_csv(os.path.join('data', 'type', 'train_data.csv'), encoding='utf-8')
-df_test = pd.read_csv(os.path.join('data', 'type', 'test_data.csv'), encoding='utf-8')
+df_train = pd.read_csv(os.path.join('data', 'train_n.csv'), encoding='utf-8')
+df_test = pd.read_csv(os.path.join('data', 'test_data.csv'), encoding='utf-8')
 
-sentences_train = df_train['text'].values + df_train['title'].values
-sentences_test = df_test['text'].values + df_test['title'].values
-y_train = df_train['type'].values
+cv = CountVectorizer()
+analyzer = cv.build_analyzer()
 
-# sentences_train, sentences_test, y_train, y_test = train_test_split(
-#         sentences, y, test_size=0.25, random_state=1000)
+sentences = df_train['text'].values
+y = df_train['type'].values
+classes = list(set(y))
 
-vectorizer = CountVectorizer()
-vectorizer.fit(sentences_train)
-X_train = vectorizer.transform(sentences_train)
-X_test = vectorizer.transform(sentences_test)
+# Тут разбиваю тренировочные данные в соотношении 9:1, это необходимо, чтобы проверять работоспособность сети.
+# Обычно на данных, на которых мы обучаем, при тестировании выходит намного более высокая точность, нежели на новых.
+sentences_train, sentences_score, y_train, y_score = train_test_split(sentences, y, test_size=0.1)
+sentences_test = df_test['text'].values
 
-classifier = LogisticRegression()
-classifier.fit(X_train, y_train)
-# score = classifier.score(X_test, y_test)
-# print('Accuracy for {} data: {:.4f}'.format('source', score))
+clf = Pipeline([
+    ('vect', CountVectorizer()),
+    ('tfidf', TfidfTransformer()),
+    ('clf', SGDClassifier(loss='hinge', penalty='l2',
+                          random_state=42,
+                          max_iter=100)),
+])
 
-answer = classifier.predict(X_test)
+
+clf.fit(sentences_train, y_train)
+
+train_score = clf.score(sentences_train, y_train)
+print('Accuracy for train data: {:.4f}'.format(train_score))
+
+predicted = clf.predict(sentences_score)
+print('Accuracy for score data: {:.4f}'.format(np.mean(predicted == y_score)))
+
+print(metrics.classification_report(y_score, predicted, target_names=classes))
+answer = clf.predict(sentences_test)
 df_answer = pd.DataFrame({'type':  answer})
-df_answer.to_csv('type.csv', index=True)
+df_answer.to_csv('answer.csv', index=True)
